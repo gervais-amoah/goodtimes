3074e1d14562a5d0926072e77b2b5a14471717ce
Gervais Amoah <gervaisamoah@yahoo.com>
2023-03-04 18:28:43 +0000

    add: speakers, register, schedule, images, footer

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+// we grab our header and our desktop header
+// we insert the contents of our header into the desktop one
+const header = document.querySelector('.header');
+const desktopHeader = document.querySelector('.header-desktop');
+desktopHeader.innerHTML = header.innerHTML;
+
+// 1. when the .header enters the viewport, hide the desktop header (removing the visible class)
+// 2. when the header leaves, show it (by adding the visible class)
+inView('.header')
+  .on('enter', el => desktopHeader.classList.remove('visible'))
+  .on('exit', el => desktopHeader.classList.add('visible'));
+
+// here we enable the tilt libary on all our images
+VanillaTilt.init(document.querySelectorAll('.image'), {
+  max: 25,
+  speed: 400,
+});
+
+// here we grab all our images we want to fade in
+// we add the visible class which toggles the opacity
+inView('.fade')
+  .on('enter', img => img.classList.add('visible'))
+  .on('exit', img => img.classList.remove('visible'));
